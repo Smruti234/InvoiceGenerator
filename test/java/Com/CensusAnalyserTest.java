ebc856724fbe51184d7c16ff2b60651ae9a058e1
Smruti <lenkasmruti123@gmail.com>
2021-11-14 06:58:44 +0530

    Added:Uc1_AnalyserLoad

---
+++ test/java/Com/CensusAnalyserTest.java
@@ -0,0 +1,50 @@
+package Com;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CensusAnalyserTest {
+    private static final String INDIA_CENSUS_CSV_FILEPATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String INDIA_CENSUS_CSV_WRONGDELIMITER = "./src/test/resources/IndiaStateCensusDataWrongDelimiter.csv";
+
+    @Test
+    public void givenIndiaCensusCsvFile_ReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILEPATH);
+            Assert.assertEquals(29, numOfRecords);
+        }catch (CensusAnalyserException e){ }
+    }
+
+    @Test
+    public void givenWrongIndiaCensusCsvFile_ReturnsException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(WRONG_FILE_PATH);
+            Assert.assertEquals(29, numOfRecords);
+        }catch (CensusAnalyserException e){
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenWrongDelimiter_ReturnCustomException(){
+        try{
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_WRONGDELIMITER);
+        }catch (CensusAnalyserException e){
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES,e.type);
+        }
+    }
+
+    @Test
+    public void givenIncorrectCsvHeader_ShouldReturnCustomException(){
+        try{
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_WRONGDELIMITER);
+        }catch (CensusAnalyserException e){
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES,e.type);
+        }
+    }
+}
